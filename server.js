e6562f44d85e22d82c789eecc7fd4e566a5dad98
unknown <alirezaabadi2016@gmail.com>
2025-04-08 15:50:27 +0330

    upgrade api

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const http = require("http");
 const fs = require("fs");
+const url = require("url");
+const db = require("./db.json");
+console.log(db);
+
 const server = http.createServer((req, res) => {
   if (req.method === "GET" && req.url === "/api/users") {
     fs.readFile("db.json", (err, data) => {
@@ -26,6 +30,24 @@ const server = http.createServer((req, res) => {
       res.write(JSON.stringify(db));
       res.end();
     });
+  } else if (req.method === "DELETE") {
+    const parsedUrl = url.parse(req.url, true);
+
+    const bookId = parsedUrl.query.id;
+    const newBook = db.books.filter((book) => book.id != bookId);
+
+    fs.writeFile(
+      "db.json",
+      JSON.stringify({ ...db, books: newBook }),
+      (err) => {
+        if (err) throw err;
+        console.log("Data has been written to file");
+
+        res.writeHead(200, { "Content-Type": "application/json" });
+        res.write(JSON.stringify({ message: "Book deleted successfully" }));
+        res.end("test response");
+      }
+    );
   }
 });
 
